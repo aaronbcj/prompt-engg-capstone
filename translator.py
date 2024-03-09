6a69b5bfe388806c00674d8e24f2bbdf154b48f8
Aaron Balson <aaronbcj@yahoo.co.in>
2024-03-09 19:19:32 +1100

    changes for streamlit cloud

--- translator.py
+++ translator.py
@@ -5,6 +5,7 @@ from playsound import playsound
 from PyPDF2 import PdfReader
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
+import base64
 
 class Translator:
   def __init__(self, from_text, lang):
@@ -40,21 +41,37 @@ class TextToSpeech:
   def text_to_speech(self):
     # Save the audio file
     tts = gTTS(self.text, lang=self.lang)
-    tts.save("output.mp3")
+    filename = "output.mp3"
 
-    # Play the audio file
-    playsound("output.mp3")
+    tts.save(filename)
+
+    # Play the audio file in windows
+    # playsound(filename)
+
+    # Play the audio file in streamlit cloud
+    with open(filename, "rb") as f:
+      data = f.read()
+      b64 = base64.b64encode(data).decode()
+      md = f"""
+          <audio controls autoplay="true">
+          <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+          </audio>
+          """
+      st.markdown(
+          md,
+          unsafe_allow_html=True,
+      )
+
     # and remove to avoid permission error next time
-    os.remove("output.mp3")
-
-
-
+    os.remove(filename)
 
 def submittext():
   st.session_state['from_text'] = st.session_state['textinput']
+  #st.session_state['textinput'] = ""
+  
 
-def submitpdf():
-  st.session_state['from_text'] = st.session_state['pdfinput']
+#def submitpdf():
+#  st.session_state['from_text'] = st.session_state['pdfinput']
 
 def main():
   # Streamlit UI
@@ -64,8 +81,10 @@ def main():
   
     # default values
     lang = "Spanish"
+    from_text = "hello my name is aaron. how are you doing today?"
+
     if 'from_text' not in st.session_state:
-      st.session_state['from_text'] = "hello my name is aaron. how are you doing today?"
+      st.session_state['from_text'] = ''
 
     # if testing, set to True
     testing = False
@@ -74,24 +93,26 @@ def main():
       # get info from user
       lang = st.selectbox('Which language do you want to translate into?',('Spanish', 'Mandarin', 'Tamil', 'Hindi'))
       
-      #todo: perhaps toggle textarea and pdf?
-      pdf = st.file_uploader("Choose to upload your pdf",key="pdfinput",  type="pdf")
-      if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        pdf_text = ""
-        for page in pdf_reader.pages:
-          pdf_text+= page.extract_text()
-          if(len(pdf_text)>300):
-            pdf_text = pdf_text[:300]
-            st.session_state['from_text'] = pdf_text
-            break
-      
-      textinput = st.empty()
-      textinput.text_area("or, paste your text for translation: ", key="textinput", value=st.session_state['from_text'], height=150, max_chars=300, on_change=submittext) 
-      
-    #label = st.empty()
-    #label.markdown(body=st.session_state['from_text'], unsafe_allow_html=False)
-
+      if(st.toggle('Upload PDF?')):
+        st.session_state['from_text']=''
+        pdf = st.file_uploader("Choose to upload your pdf",key="pdfinput", type="pdf")
+        if pdf is not None:
+          pdf_reader = PdfReader(pdf)
+          pdf_text = ""
+          for page in pdf_reader.pages:
+            pdf_text+= page.extract_text()
+            if(len(pdf_text)>300):
+              pdf_text = pdf_text[:300]
+              st.session_state['from_text'] = pdf_text
+              st.markdown(body=st.session_state['from_text'], unsafe_allow_html=False)
+              break
+      else:
+        #st.session_state['from_text']=''
+        textinput = st.empty()
+        textinput.text_area("Paste your text for translation: ", key="textinput", value=st.session_state['from_text'], height=150, max_chars=300, on_change=submittext) 
+        st.markdown(body=st.session_state['from_text'], unsafe_allow_html=False)
+    
+    
     if st.button("Translate") or testing:
       if st.session_state['from_text']:
         
@@ -107,10 +128,9 @@ def main():
           # Play the audio
           tts = TextToSpeech(to_text, lang)
           tts.text_to_speech()
-
-          st.button("Play audio again", on_click=tts.text_to_speech)
+          
       else:
-          st.warning("Please paste a text to translate.")
+          st.warning("Please input a text to translate.")
   except Exception as e:
     st.error("Error has occurred. Please try again.")
     print(e)
